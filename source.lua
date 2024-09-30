16305cfb7243e32a16d7f2457aac7123a32b59d9
weirdo123234 <charlesarieger@icloud.com>
2024-09-29 20:23:37 -0600

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,24 @@
+    workspace.Mouse:Destroy()
+    workspace.NewMouse:Destroy()
+    workspace.NewRatLocation:Destroy()
+    workspace.TeleportPointNewRat:Destroy()
+
+local Plugin = {
+    ["PluginName"] = "Cheese Escape Rat Remover",
+    ["PluginDescription"] = "Deletes rat!",
+    ["Commands"] = {
+        ["removerat"] = {
+            ["ListName"] = "monsterESP (Run Every Round Or It Might Delete)",
+            ["Description"] = "Highlights the monsters (Run Every Round)",
+            ["Aliases"] = {"deleterat"},
+            ["Function"] = function(args,speaker)
+    workspace.Mouse:Destroy()
+    workspace.NewMouse:Destroy()
+    workspace.NewRatLocation:Destroy()
+    workspace.TeleportPointNewRat:Destroy()
+            end
+        }
+    }
+}
+
+return Plugin
